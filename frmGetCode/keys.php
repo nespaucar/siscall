169cd2381f126d61545a93a301169b97daa8fea7
nespaucar <nespaucar@gmail.com>
2020-09-05 21:57:06 -0500

    changes

--- frmGetCode/keys.php
+++ frmGetCode/keys.php
@@ -1,5 +1,13 @@
 <?php 
+	// CAPTCHA LOCALHOST
 	define('SITE_KEY', '6LdtMMgZAAAAAFmKiUXr1uoCz4KErrAYm4daU9w-');
 	define('SECRET_KEY', '6LdtMMgZAAAAAKz2BAGQwnHMfgd36BLpZy0gYVw5');
+	// CAPTCHA toditopati
+	//define('SITE_KEY', '6LenVsgZAAAAAG6GnsgsZGCI3wLqwfMXU9_Qo8bd');
+	//define('SECRET_KEY', '6LenVsgZAAAAAMuhMaSJnVtogIBftqcmJnjpBsRV');
+	// CAPTCHA 165.232.118.94
+	//define('SITE_KEY', '6LfVPcgZAAAAAGkeMh1qAdLfg_cCmzGOp2m9pXWD');
+	//define('SECRET_KEY', '6LfVPcgZAAAAAPqt6pTKW2evSkKkBvDIbKIN43dG');
+	//Twilio
 	define('TWILIO_ID', 'ACb3ac0062060a8cecf603131ad29d22d1');
 	define('TWILIO_TOKEN', '0da8c5d4b796af060269cc524c1ece28');
